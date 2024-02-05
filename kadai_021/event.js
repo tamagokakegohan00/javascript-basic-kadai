d88d8332db89a61e90d4d354ad582ba79b415cc1
tamagokakegohan00 <144686432+tamagokakegohan00@users.noreply.github.com>
2024-02-05 19:54:17 +0900

    kadai_021提出

---
+++ kadai_021/event.js
@@ -0,0 +1,14 @@
+console.log(document.body)
+
+//const btn =document.getElementById(btn);
+
+btn.addEventListener("click",()=>{
+    setTimeout(()=>{
+    text.textContent="ボタンをクリックしました";
+  },2000);
+});
+
+
+
+
+
