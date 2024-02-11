2cfee0264392ad5006d1f8d065ade134d9771908
tamagokakegohan00 <144686432+tamagokakegohan00@users.noreply.github.com>
2024-02-11 21:15:53 +0900

    kadai_009 再提出

--- kadai_009/holiday.js
+++ kadai_009/holiday.js
@@ -1,7 +1,15 @@
 let holidays = ["正月","成人の日","建国記念日","天皇誕生日","春分の日","昭和の日","憲法記念日","みどりの日","こどもの日","海の日","山の日","敬老の日","秋分の日","スポーツの日","文化の日","勤労感謝の日"];
 
+
 for (let i=0 ; i <=15 ; i++) {
   console.log(holidays[i]);
 }
 
 
+
+let n = 0 ;
+
+while (n <= holidays.length ){
+  n++ ;
+ console.log(holidays[n]);
+}
